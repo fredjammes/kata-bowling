15b0f133eea2df117d04d5f65f1c32304ee04415
Frédéric JAMMES <25798737+fredjammes@users.noreply.github.com>
2025-07-30 14:31:37 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/a-first-test.spec.ts

--- src/a-first-test.spec.ts
+++ src/a-first-test.spec.ts
@@ -1,12 +1,36 @@
-import { describe, expect, test } from 'vitest'
+import { describe, expect, test } from 'vitest';
 
-describe("a first test", () => {
-  test('A test that works', () => {
-    expect(1+2).toBe(3)
-  })
+class Game {
+  private rolls: number[] = [];
+  roll(number: number) {
+    this.rolls.push(number);
+  }
 
-  test('A test that fails', () => {
-    expect(1+2).toBe(4)
+  score() {
+    return this.rolls[0] + this.rolls[1] + this.rolls[2] + this.rolls[3] + this.rolls[4] + this.rolls[5] + this.rolls[6] + this.rolls[7] + this.rolls[8] + this.rolls[9]
+      + this.rolls[10] + this.rolls[11] + this.rolls[12] + this.rolls[13] + this.rolls[14] + this.rolls[15] + this.rolls[16] + this.rolls[17] + this.rolls[18] + this.rolls[19];
+  }
+}
+
+describe('', () => {
+  test('', () => {
+    const game: Game = new Game();
+    for (let i = 0; i < 20; i++)
+      game.roll(1);
+    expect(game.score()).toBe(20);
   });
-
+  test('', () => {
+    const game: Game = new Game();
+    for (let i = 0; i < 20; i++)
+      game.roll(2);
+    expect(game.score()).toBe(40);
+  });
+  test('', () => {
+    const game: Game = new Game();
+    game.roll(9);
+    game.roll(1);
+    for (let i = 0; i < 18; i++)
+      game.roll(1);
+    expect(game.score()).toBe(29);
+  });
 });
